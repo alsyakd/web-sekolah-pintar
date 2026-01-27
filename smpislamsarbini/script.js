0b15e42bcfcdd7fcc448103ba35058bbe9bb29bc
ilhamkf13 <kukuhilham01@gmail.com>
2026-01-27 16:39:12 +0700

    done semua bang kurang video sama e-learning

--- smpislamsarbini/script.js
+++
@@ -1,9 +0,0 @@
-// Efek header saat scroll
-window.addEventListener('scroll', function () {
-  const header = document.getElementById('navbar');
-  if (window.scrollY > 50) {
-    header.style.background = 'rgba(26, 58, 58, 1)';
-  } else {
-    header.style.background = 'rgba(26, 58, 58, 0.95)';
-  }
-});
