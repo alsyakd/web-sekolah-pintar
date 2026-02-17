7151d9aea186e2989a8a3c04d4b10c91f4ea9b7e
RestuWibisonoo <restu.wibisonoo@gmail.com>
2026-02-17 19:45:02 +0700

    update

--- mimaarifgrabag1/index.php
+++ mimaarifgrabag1/index.php
@@ -438,7 +438,7 @@
             spans[1].style.opacity = navMenu.classList.contains("active") ? "0" : "1";
             spans[2].style.transform = navMenu.classList.contains("active") ? "rotate(-45deg) translate(5px, -6px)" : "none";
         });
-
+ 
         // Active menu
         const sections = document.querySelectorAll("section");
         const navLinks = document.querySelectorAll("nav ul li a");
